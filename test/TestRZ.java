d8cbf63cf0ec26f8ecb28957e97bb61b6afed575
PolinaRubinova <jerrygonsales@mail.ru>
2021-02-26 21:59:43 +0300

    rz1

---
+++ test/TestRZ.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class TestRZ {
+
+    @Test
+    void test_RZ_1_info_messages_ex1() {
+        Assert.assertEquals(0, RZ_1_info_messages.ex1());
+    }
+
+    @Test
+    void test_RZ_1_info_messages_ex2() {
+        Assert.assertEquals(0, RZ_1_info_messages.ex2());
+    }
+
+}
