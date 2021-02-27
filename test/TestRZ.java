51d98952e291447ccdb17b408054f18f3f56b8c5
PolinaRubinova <jerrygonsales@mail.ru>
2021-02-27 04:04:28 +0300

    rz1

--- test/TestRZ.java
+++ test/TestRZ.java
@@ -1,10 +1,12 @@
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 public class TestRZ {
 
     @Test
-    void test_RZ_1_info_messages_ex1() {
+    void test_RZ_1_info_messages_ex1() throws IOException {
         Assert.assertEquals(0, RZ_1_info_messages.ex1());
     }
 
